72bdcbd25131e8f19bfc17c0be50e4335cc523b7
nerdale <alexandra.neira@gmail.com>
2017-10-22 16:04:22 -0300

    subiendo ejercicio fibonacci recursivo

--- homework/index.js
+++ homework/index.js
@@ -3,7 +3,7 @@ exports.factorial = (n) => {
 }
 
 exports.fibonacci = (n) => {
-
+	const fibonacci = n => n == 1 || n == 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 exports.divide = (a, b) => {
