0f67eb82182748aea9a57ff32701fcc73b6ead5a
nerdale <alexandra.neira@gmail.com>
2017-10-22 16:26:00 -0300

    arreglando sintaxis ejercicios recursivos para que pasen test

--- homework/index.js
+++ homework/index.js
@@ -1,17 +1,22 @@
 exports.factorial = (n) => {
-	const factorial = n => n == 0 ? 1 : n * factorial(n - 1)
+	const factorial = n == 0 ? 1 : n * exports.factorial(n - 1)
+	return factorial
+	module.exports = exports.factorial;
 }
 
 exports.fibonacci = (n) => {
-	const fibonacci = n => n == 1 || n == 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
+	const fibonacci = n == 1 || n == 2 ? 1 : exports.fibonacci(n - 1) + exports.fibonacci(n - 2)
+	return fibonacci
+	module.exports = exports.fibonacci
 }
 
 exports.divide = (a, b) => {
-	const divide = (a, b) => b > a ? 0 : divide(a - b, b) + 1
+	const divide = b > a ? 0 : exports.divide(a - b, b) + 1
+	return divide
+	module.exports = exports.divide
 }
 
 exports.invert = (n) => {
-
 }
 
 exports.suma = (n) => {
@@ -26,4 +31,11 @@ exports.sumArr = (arr) => {
 
 }
 
+function invert(n){
+	if(n < 10){
+		return n;
+	}else {
+		return (n%10) + invert(n / 10) * 10;
+	}
+}
 
