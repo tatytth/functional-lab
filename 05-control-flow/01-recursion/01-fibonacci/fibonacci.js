ed7e9290ab65b4994b69c3826bf06a872ce08314
nerdale <alexandra.neira@gmail.com>
2017-10-23 17:29:38 -0300

    agregando fibonacci

--- 05-control-flow/01-recursion/01-fibonacci/fibonacci.js
+++ 05-control-flow/01-recursion/01-fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = n => {
+	if(n===0){
+		return 0
+	}
+	else if(n===1){
+		return 1
+	}
+	else{
+		return (fibonacci2(n-2)+fibonacci2(n-1))
+	}
+}
 
+module.exports = fibonacci
 }
 
 module.exports = fibonacci
